84abfb675917d83bbaa5341fe67d82acf873313f
JaeHee <jaeheej.nextacademy@gmail.com>
2021-03-10 20:24:34 -0500

    My First Xcode Git commit

---
+++ Expiramental Project Xcode and GitHub Relate Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expiramental Project Xcode and GitHub Relate Project
+//
+//  Created by Jae on 3/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
